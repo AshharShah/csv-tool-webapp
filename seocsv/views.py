d20bd7ce8fc4be8f31e04a900da19da811573c13
AshharShah <ashhar.shah786@gmail.com>
2022-08-31 12:57:23 +0500

    Finalized by removing API and adding Beautifulsoup4

--- seocsv/views.py
+++ seocsv/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 import urllib.request
 import json
 from django.views.decorators.csrf import csrf_exempt
+import requests
+from bs4 import BeautifulSoup
 
-from serpapi import GoogleSearch
+#from serpapi import GoogleSearch
 import pandas as pd
 
 # Create your views here.
@@ -18,11 +20,6 @@ def index(request):
 @csrf_exempt
 def output(request):
     print("posted")
-    # url = 'https://api.dictionaryapi.dev/api/v2/entries/en_US/' + request.POST['user_input']
-    # response = urllib.request.urlopen(url)
-    # result = json.loads(response.read())
-    # context = {'word':result[0]['word'], 'meanings':result[0]['meanings']}
-    # return render(request, 'output.html', context)
     print(type(request.FILES['file']))
     file = pd.read_csv(request.FILES['file'])
     file = file.iloc[:, [0, 1]]
@@ -39,25 +36,17 @@ def output(request):
     # lop to get data against each keyword in the dataframe
     for i in range(keys.shape[0]):
         keyToSearch = str(keys[i])
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"}
+        URL     = "https://www.google.com/search?q=Allintitle:" + str(keyToSearch)
+        result = requests.get(URL, headers=headers)    
+        soup = BeautifulSoup(result.content, 'html.parser')
+        total_results_text = soup.find("div", {"id": "result-stats"}).find(text=True, recursive=False) # this will give you the outer text which is like 'About 1,410,000,000 results'
+        results_num = ''.join([num for num in total_results_text if num.isdigit()]) # now will clean it up and remove all the characters that are not a number .
         print("Keyword: " + keyToSearch)
         print("Values: " + str(values[i]))
-        params = {
-            "q": keyToSearch,
-            "hl": "en",
-            "gl": "us",
-            "google_domain": "google.com",
-            "output": "JSON",
-            "api_key": "de33899a76f4499a591a33eab0383513a713a19c86c7e7f16685e829c2c49989"
-        }
-        # making a api call to search the provided paramater data
-        search = GoogleSearch(params)
-        dict_results = search.get_dict()
-        # filtering the data to get the required variable that is the total number of searches
-        d1 = dict_results["search_information"]
-        d2 = d1["total_results"]
-        print("Total Results: " + str(d2))
-        totResults.append(int(d2))
-        kgr = d2/values[i]
+        print("Total Results: " +str(results_num))
+        totResults.append(int(results_num))
+        kgr = int(results_num)/values[i]
         print("KGR: " + str(kgr) + "\n\n")
         kgrs.append(round(float(kgr), 2))
     print("The List Of Results: ")
